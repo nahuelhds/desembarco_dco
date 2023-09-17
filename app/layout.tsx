000c0d78248e39830ace73721d1719b0b10c8845
Nahue <nahuelhds@gmail.com>
2023-09-17 13:11:50 -0300

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,36 @@ import { fontDMSans } from "@/app/fonts";
 export const metadata: Metadata = {
   title: "Miedo al éxito | Lo relevante",
   description: "Miedo al éxito | Lo relevante",
+  openGraph: {
+    title: "Miedo al éxito | Lo relevante",
+    description: "Miedo al éxito | Lo relevante",
+    images: ["/img/metadata-logo.jpg"],
+  },
+  twitter: {
+    title: "Miedo al éxito | Lo relevante",
+    description: "Miedo al éxito | Lo relevante",
+    images: ["/img/metadata-logo.jpg"],
+  },
+  appleWebApp: {
+    title: "Miedo al éxito | Lo relevante",
+    startupImage: ["/img/metadata-logo.jpg"],
+  },
+  keywords: [
+    "miedo al éxito",
+    "Lo relevante",
+    "comunidad",
+    "independiente",
+    "información",
+    "separando la señal del ruido",
+    "uruguay",
+    "uruguayo",
+    "plataforma digital",
+    "medio de comunicación",
+    "comunicación",
+    "medios",
+    "noticias",
+    "resumen semanal",
+  ],
 };
 
 export default function RootLayout({
