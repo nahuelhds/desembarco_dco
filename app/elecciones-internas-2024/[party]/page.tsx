5f283e04844123eb2116551e0ee6998d47f68ade
Nahuel Sotelo <nsotelo@dlocal.com>
2024-06-29 16:15:55 -0300

    Participación y mesas estrutadas

--- app/elecciones-internas-2024/[party]/page.tsx
+++ app/elecciones-internas-2024/[party]/page.tsx
@@ -46,7 +46,7 @@ export default function CandidatesPage({ params }: CandidatesProps) {
       )}
       <div className={"absolute bottom-2"}>
         <div className={"text-center bg-black px-4 py-2 rounded-2xl text-2xl"}>
-          <label className={"font-bold"}>Mesas escrutadas:</label>
+          <label className={"font-bold"}>Total escrutinio:</label>
           {" "}
           <span>{voteProgress.toFixed(2)}%</span>
         </div>
