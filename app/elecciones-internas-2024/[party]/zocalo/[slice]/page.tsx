83fe949980e49069f36882d5efb5c797217222b3
Nahuel Sotelo <nsotelo@dlocal.com>
2024-06-30 17:48:06 -0300

    Revert "Percentage 0"
    This reverts commit 4d1a1697ebe95e09fa2441cb9c3238b9464a84af.

--- app/elecciones-internas-2024/[party]/zocalo/[slice]/page.tsx
+++ app/elecciones-internas-2024/[party]/zocalo/[slice]/page.tsx
@@ -24,9 +24,8 @@ export default function CandidatesPage({ params }: CandidatesPageProps) {
             <div className={`candidate ${key}`} />
             <div className={"candidate-name flex flex-col justify-around px-8"}>
               <span className={"last-name uppercase"}>{lastName}</span>
-              <span>{(0).toFixed(2)}%</span>
-              {/*<strong*/}
-              {/*  className={"percentage"}>{(voteCount * 100 / totalPartyCount).toFixed(ZOCALO_FIXED_DECIMALS)}%</strong>*/}
+              <strong
+                className={"percentage"}>{(voteCount * 100 / totalPartyCount).toFixed(ZOCALO_FIXED_DECIMALS)}%</strong>
             </div>
           </div>
         )}
